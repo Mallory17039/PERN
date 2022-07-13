be55c0c5b3c8fd9a45c7e4f7cd9f36c470881a82
Mallory Lee <sein980406@gmail.com>
2022-07-13 02:26:38 -0600

    updated

--- server/database.sql
+++ server/database.sql
@@ -4,3 +4,11 @@ CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     desciption VARCHAR(250)
 )
+
+INSERT INTO todo(todo_id, description)
+VALUES 
+	(1, 'Wash the car'),
+	(2, 'Water the flower'),
+	(3, 'Pick up Mallory at the gym'),
+	(4, 'Make a dinner'),
+	(5, 'Make a doctor appointment');
--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
+
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 //middleware
 app.use(cors());
@@ -8,5 +10,15 @@ app.unsubscribe(express.json());
 
 app.listen(5000, () => {
     console.log("server has started on port 5000");
+});
+
+app.get("/todoItems", async(req, res) => {
+  // get todo items
+  try {
+	const allTodos = await pool.query("SELECT * FROM todo");
+	res.json(allTodos.rows)
+	} catch (err) {
+	console.error(err.message);
+ }
 });
 
